ca552ddbd5a1aba456a7a1c4d598b7044fa7db3f
m23 <maguire25xgaming@gmail.com>
2023-08-29 15:53:58 +0100

    i think this is the final commit oh my god

--- assets/preload/stages/void.lua
+++ assets/preload/stages/void.lua
@@ -4,7 +4,12 @@ function onCreate()
 	setScrollFactor('white', 0.9, 0.9);
 	scaleObject('white', 1.5, 1.5);
 
+	makeLuaSprite('help', 'rip', 0, 0);
+	setScrollFactor('help', 0.9, 0.9);
+	setObjectCamera('help', 'other');
+
 	addLuaSprite('white', false);
+	addLuaSprite('help', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
